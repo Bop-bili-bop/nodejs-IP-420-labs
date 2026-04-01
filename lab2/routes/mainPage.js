785a21f323a137b3a99837f2677de094e56e608c
imcheater124 <imcheeter124@gmail.com>
2026-04-01 14:56:16 +0300

    add team member  profiles and update main and student pages

--- lab2/routes/mainPage.js
+++ lab2/routes/mainPage.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { teamMembers } = require('./data');
 
 router.get("/", (req, res) => {
-    res.render('index');
+    res.render('index', { teamMembers });
 });
 
 module.exports = router;
